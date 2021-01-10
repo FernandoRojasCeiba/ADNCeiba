b0d198e562285d115b336753877659e8a2811124
Luis Fernando Rojas Ortiz - Ceiba Software <fernando.rojas@ceiba.com.co>
2021-01-10 14:48:21 -0500

    se agregan pruebas de integracion

--- microservicio/infraestructura/src/test/java/com/ceiba/cita/controlador/ConsultarControladorCitaTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/cita/controlador/ConsultarControladorCitaTest.java
@@ -20,6 +20,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ConsultaControladorCita.class)
 public class ConsultarControladorCitaTest {
 
+    private static final Long ID_CITA = 1L;
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -32,5 +34,17 @@ public class ConsultarControladorCitaTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void  consultarCitasPorCedula() throws Exception{
+       // Arrange
+
+        String cedula = "1036946621";
+
+        // Act - Assert
+        mockMvc.perform(get("/consultas/{cedula}", cedula)
+        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
